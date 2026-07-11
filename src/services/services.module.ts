9e9f7a54f849e05da67ef8f737996f017a0a3a0c
hirunaOusl <139805836+hirunaOusl@users.noreply.github.com>
2026-07-11 10:57:27 +0530

    feat: update ServicesModule to include TypeOrmModule for Service entity

--- src/services/services.module.ts
+++ src/services/services.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ServicesController } from './services.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ServicesService } from './services.service';
+import { ServicesController } from './services.controller';
+import { Service } from './entities/service.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Service])],
   controllers: [ServicesController],
-  providers: [ServicesService]
+  providers: [ServicesService],
+  exports: [ServicesService], // Export so BookingsModule can check service states
 })
 export class ServicesModule {}
